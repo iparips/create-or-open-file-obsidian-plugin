bb1ddd28558c3dc4a470c2e7830ac6534923102f
Ilya Paripsa <iparips@gmail.com>
2025-03-10 16:47:46 +1100

    Add prettier

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,42 @@
 // import { App, Modal, Plugin } from 'obsidian';
-import { Plugin } from 'obsidian';
-import { SampleSettingTab } from './settings/settingsTab';
-import { DEFAULT_SETTINGS, PluginSettings } from './settings/constants';
-import { NoteCreator } from './noteCreator';
+import { Plugin } from 'obsidian'
+import { SampleSettingTab } from './settings/settingsTab'
+import { DEFAULT_SETTINGS, PluginSettings } from './settings/constants'
+import { NoteCreator } from './noteCreator'
 
 export default class MyPlugin extends Plugin {
-	settings: PluginSettings;
+	settings: PluginSettings
 
 	async onload() {
-		await this.loadSettings();
+		await this.loadSettings()
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-or-create-this-weeks-shopping-list',
-			name: 'Open or create this week\'s shopping list',
+			name: "Open or create this week's shopping list",
 			callback: async () => {
-				await new NoteCreator(this.app).openOrCreateShoppingListFile();
-			}
-		});
+				await new NoteCreator(this.app).openOrCreateShoppingListFile()
+			},
+		})
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SampleSettingTab(this.app, this))
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
+			console.log('click', evt)
+		})
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
+		await this.saveData(this.settings)
 	}
 }
 
@@ -58,4 +55,3 @@ export default class MyPlugin extends Plugin {
 // 		contentEl.empty();
 // 	}
 // }
-
