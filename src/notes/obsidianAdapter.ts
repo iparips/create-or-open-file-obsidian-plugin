87500e4ea65afefe13654cc6e103842a1a292faf
Ilya Paripsa <313095+iparips@users.noreply.github.com>
2025-04-28 21:14:33 +1000

    Allow empty template file

--- src/notes/obsidianAdapter.ts
+++ src/notes/obsidianAdapter.ts
@@ -1,4 +1,4 @@
-import type { App, TFile } from 'obsidian'
+import type { App } from 'obsidian'
 
 export class ObsidianAdapter {
 	app: App
@@ -14,31 +14,34 @@ export class ObsidianAdapter {
 	 * returns a promise with string representing message to user
 	 * 	about the operation
 	 *  */
-	public async openFile(filePath: string, sourcePath = ""): Promise<string> {
+	public async openFile(filePath: string, sourcePath = ''): Promise<string> {
 		return this.app.workspace
 			.openLinkText(filePath, sourcePath)
 			.catch(() => Promise.reject(`Could not open the filePath [${filePath}], sourcePath[${sourcePath}]`))
-			.then(() => Promise.resolve('File opened successfully'))
+			.then(() => Promise.resolve('Note opened'))
 	}
 
 	public doesFileExist(fullPath: string): boolean {
 		return this.app.vault.getFileByPath(fullPath) !== null
 	}
 
-	public async createFileAndFolder(filePath: string, templateFilePath: string): Promise<TFile> {
+	public async createFileAndFolder(filePath: string, templateFilePath?: string): Promise<string> {
 		const folderPath = filePath.substring(0, filePath.lastIndexOf('/'))
-		const templateFile = this.app.vault.getFileByPath(templateFilePath)
-		if (!templateFile) {
-			return Promise.reject('Template file not found')
-		}
-
 		const noteFolderExists = this.app.vault.getFolderByPath(folderPath)
 		if (!noteFolderExists) {
 			await this.app.vault.createFolder(folderPath)
 		}
-
-		return this.app.vault
-			.read(templateFile)
+		return this.getTemplateContentOrEmpty(templateFilePath)
 			.then((templateContent: string) => this.app.vault.create(filePath, templateContent))
+			.then(() => Promise.resolve("Note created"))
+	}
+
+	private getTemplateContentOrEmpty(templateFilePath?: string): Promise<string> {
+		return templateFilePath ? this.loadTemplateContent(templateFilePath) : Promise.resolve('')
+	}
+
+	private loadTemplateContent(templateFilePath: string): Promise<string> {
+		const templateFile = this.app.vault.getFileByPath(templateFilePath)
+		return templateFile ? this.app.vault.read(templateFile) : Promise.reject('Template file not found')
 	}
 }
